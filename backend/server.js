16095c365463c456cd318bee995314fe34ac9e8d
Philip <p.s.bebbington@gmail.com>
2019-01-30 14:43:52 +0000

    Changed stuff

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,12 @@ app.get('/api/me', (request, response) => {
 });
 
 app.get('/api/work', (request, response) => {
-	response.send('[{"yearFrom": "2010", "yearTo": "2018", "place": "BBC Radio", "comment": "Was doing stuff"}, {"yearFrom": "2009", "yearTo": "2010", "place": "The coffee shop", "comment": "Making coffee"}]');
+	response.send('[{"yearFrom": "2016", "yearTo": "2019", "place": "Berrywordl", "comment": "Doing as little stuff as possible"}, {"yearFrom": "2009", "yearTo": "2010", "place": "The coffee shop", "comment": "Making coffee"}]');
 	logger.info('Responded to GET request on /api/work')
 });
 
 app.get('/api/education', (request, response) => {
-	response.send('[{"yearFrom": "2005", "yearTo": "2007", "place": "University 1", "comment": "Studying"}, {"yearFrom": "2001", "yearTo": "2004", "place": "Barista academy", "comment": "Made coffee"}]');
+	response.send('[{"yearFrom": "2005", "yearTo": "2009", "place": "University of Hertfordshire", "comment": "\"Studying\""}, {"yearFrom": "2001", "yearTo": "2004", "place": "Barista academy", "comment": "Made coffee"}]');
 	logger.info('Responded to GET request on /api/edu')
 });
 
